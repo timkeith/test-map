deafba960eb7dcce907901391b814fb97329a31f
Tim Keith <tim@tkeith.com>
2026-02-18 16:18:13 -0800

    Add checkboxes for layers

--- js/map.js
+++ js/map.js
@@ -7,6 +7,7 @@ function initMap() {
     document.getElementById('map'),
     { center: { lat: 0, lng: 0 }, zoom: 2 });
   bounds = new google.maps.LatLngBounds();
+  const container = document.getElementById('layers');
   for (const [name, url] of Object.entries(layerFiles)) {
     const layer = new google.maps.KmlLayer({
       url:              `${window.location.origin}${window.location.pathname}${url}`,
@@ -16,6 +17,20 @@ function initMap() {
       bounds.union(layer.getDefaultViewport());
       map.fitBounds(bounds);
     });
+
+    // Checkbox UI
+    const label = document.createElement('label');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = true;
+    checkbox.onchange = () => {
+      layer.setMap(checkbox.checked ? map : null);
+    };
+
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode(' ' + name));
+    container.appendChild(label);
+    container.appendChild(document.createElement('br'));
     layer.setMap(map);
   }
 }

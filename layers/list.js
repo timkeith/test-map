2d86b1c2dd892f87c6e39ec2b6751e8a29d84621
Tim Keith <tim@tkeith.com>
2026-03-17 17:44:27 -0700

    from deploy.py

--- layers/list.js
+++ layers/list.js
@@ -1,5 +1,8 @@
-// change version number when file changes to defeat caching
 const layerFiles = {
-  "Hotels": "layers/places.kml?v=1",
-  "Routes": "layers/routes.kml?v=1"
+  "Hotels 2": "layers/places.kml?v=2",
+  "Bike Routes": "layers/routes.kml?v=2",
+  "Hotels 1": "layers/hotels.kml?v=1",
+  "Ride With GPS Routes": "layers/rwgps.kml?v=1",
+  "Near Termini": "layers/drive.kml?v=1",
+  "1-star Restaurants": "layers/map.kml?v=1"
 };

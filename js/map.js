6a9c44cb57c85da3ad6ad90c4a2f57a7311da34a
Tim Keith <tim@tkeith.com>
2026-03-23 06:42:38 -0700

    Fix box for legend

--- js/map.js
+++ js/map.js
@@ -2,13 +2,11 @@ let map;
 let bounds;
 
 function initMap() {
-  // temp center & zoom, set later by fitBounds
   map = new google.maps.Map(
     document.getElementById('map'),
-    { center: { lat: 0, lng: 0 }, zoom: 2 });
+    { center: { lat: 0, lng: 0 }, zoom: 2 });  // center & zoom set later by fitBounds
   bounds = new google.maps.LatLngBounds();
   const container = document.getElementById('layers');
-  console.log('container:', container);
   for (const [name, url] of Object.entries(layerFiles)) {
     const layer = new google.maps.KmlLayer({
       url:              `${window.location.origin}${window.location.pathname}${url}`,

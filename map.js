8a685cb403b5699eacc07109b3430c5fd1538f77
Tim Keith <tim@tkeith.com>
2026-03-27 13:16:34 -0700

    Add restaurants.

--- map.js
+++ map.js
@@ -15,7 +15,6 @@ function initMap() {
 
 function buildTree(data, parentElement) {
   const baseUrl = new URL('./', window.location.href).href;
-  console.log('baseUrl:', baseUrl);
   const ul = document.createElement('ul');
   data.forEach(item => {
     const li = document.createElement('li');
@@ -54,7 +53,6 @@ function buildTree(data, parentElement) {
     } else {
       li.appendChild(labelContainer);
       const url = new URL(item.url, baseUrl).href;
-      console.log('url:', url);
       const layer = new google.maps.KmlLayer({ url: url, preserveViewport: true, map: map });
       google.maps.event.addListenerOnce(layer, 'status_changed', () => {
         if (layer.getStatus() === google.maps.KmlLayerStatus.OK) {
